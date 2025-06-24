eebfc50d21cc89d7dbf27a0c0f35995e256efae5
Fernando Luiz Nery Oechsler <113445964+fernando-oechsler@users.noreply.github.com>
2025-06-24 10:49:05 -0300

    Update msa620.c

--- msa620.c
+++ msa620.c
@@ -1,121 +1,211 @@
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/mod_devicetable.h>
-#include <linux/property.h>
 #include <linux/platform_device.h>
-#include <linux/of_device.h>
+#include <linux/of.h>
 #include <linux/gpio/consumer.h>
-#include <linux/proc_fs.h>
+#include <linux/kthread.h>
+#include <linux/input.h>
 
-/* Meta Information */
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("fernando-oechsler");
-MODULE_DESCRIPTION("teste");
+MODULE_AUTHOR("Fernando Oechsler");
+MODULE_DESCRIPTION("Driver teclado MSA620 com thread, input, debounce e Led de status");
+MODULE_VERSION("1.0");
 
-/* Declate the probe and remove functions */
-static int dt_probe(struct platform_device *pdev);
-static void dt_remove(struct platform_device *pdev);
+#define NUM_ROWS 6
+#define NUM_COLS 5
 
-static struct of_device_id my_driver_ids[] = {
-	{
-		.compatible = "keyboard,msa620",
-	}, { /* sentinel */ }
-};
-MODULE_DEVICE_TABLE(of, my_driver_ids);
+/* Debounce config */
+#define DEBOUNCE_SAMPLES 4
+#define DEBOUNCE_TIME_MS 40
 
-static struct platform_driver my_driver = {
-	.probe = dt_probe,
-	.remove = dt_remove,
-	.driver = {
-		.name = "my_device_driver",
-		.of_match_table = my_driver_ids,
-	},
-};
+static struct gpio_descs *row_gpios;
+static struct gpio_descs *col_gpios;
+static struct gpio_desc *ledtec_gpio;
 
-/* GPIO variable */
-static struct gpio_desc *my_led = NULL;
+static struct input_dev *input_dev;
+static struct task_struct *keyboard_task;
 
-static struct proc_dir_entry *proc_file;
-
-/**
- * @brief Write data to buffer
- */
-static ssize_t my_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
-	switch (user_buffer[0]) {
-		case '0':
-		case '1':
-			gpiod_set_value(my_led, user_buffer[0] - '0');
-		default:
-			break;
-	}
-	return count;
-}
-
-static struct proc_ops fops = {
-	.proc_write = my_write,
+/* Key map */
+static const int key_map[NUM_ROWS][NUM_COLS] = {
+    {KEY_F5, KEY_F4, KEY_F3, KEY_F2, KEY_F1},
+    {KEY_UP, -1, KEY_3, KEY_2, KEY_1},
+    {KEY_RIGHT, KEY_LEFT, -1, -1, -1},
+    {KEY_DOWN, -1, KEY_6, KEY_5, KEY_4},
+    {KEY_ENTER, KEY_ESC, KEY_9, KEY_8, KEY_7},
+    {-1, -1 , -1, KEY_0, -1}
 };
 
-/**
- * @brief This function is called on loading the driver 
- */
-static int dt_probe(struct platform_device *pdev) {
-	struct device *dev = &pdev->dev;
+/* Debounce function */
+static bool debounce_read(struct gpio_desc *gpio) {
+    int stable_count = 0;
+    unsigned long start = jiffies;
+    unsigned long interval = msecs_to_jiffies(DEBOUNCE_TIME_MS) / DEBOUNCE_SAMPLES;
+    bool last = gpiod_get_value(gpio);
 
-	printk("dt_gpio - Now I am in the probe function!\n");
+    while (time_before(jiffies, start + msecs_to_jiffies(DEBOUNCE_TIME_MS))) {
+        bool now = gpiod_get_value(gpio);
+        if (now == last) {
+            stable_count++;
+        } else {
+            stable_count = 0;
+        }
+        last = now;
 
-	/* Check for device properties */
-	if(!device_property_present(dev, "ledtec-gpios")) {
-		printk("dt_gpio - Error! Device property 'led' not found!\n");
-		return -1;
-	}
+        if (stable_count >= DEBOUNCE_SAMPLES)
+            break;
 
-	/* Init GPIO */
-	my_led = gpiod_get(dev, "ledtec", GPIOD_OUT_LOW);
-	if (IS_ERR(my_led)) {
-    		printk("dt_gpio - Error! Could not setup the LED GPIOs\n");
-    		return PTR_ERR(my_led);
-	}
+        while (time_before(jiffies, start + interval)) {
+            cpu_relax();
+        }
+        start = start + interval;
+    }
 
-	/* Creating procfs file */
-	proc_file = proc_create("my-led", 0666, NULL, &fops);
-	if(proc_file == NULL) {
-		printk("procfs_test - Error creating /proc/my-led\n");
-		gpiod_put(my_led);
-		return -ENOMEM;
-	}
-
-
-	return 0;
+    return last;
 }
 
-/**
- * @brief This function is called on unloading the driver 
- */
-static void dt_remove(struct platform_device *pdev) {
-	printk("dt_gpio - Now I am in the remove function\n");
-	gpiod_put(my_led);
-	proc_remove(proc_file);
+/* Thread */
+static int keyboard_thread_fn(void *data) {
+    bool led_state = false;
+    unsigned long led_jiffies = jiffies;
+    int col, row;
+    bool key_pressed = false;
+
+    while (!kthread_should_stop()) {
+        key_pressed = false;
+
+        for (col = 0; col < NUM_COLS; col++) {
+            /* Ativa coluna (LOW) */
+            gpiod_set_value(col_gpios->desc[col], 0);
+
+            for (row = 0; row < NUM_ROWS; row++) {
+                int keycode = key_map[row][col];
+                if (keycode == -1)
+                    continue;
+
+                bool val = debounce_read(row_gpios->desc[row]);
+
+                if (!val) { /* Pressionado */
+                    key_pressed = true;
+                    input_report_key(input_dev, keycode, 1);
+                    input_sync(input_dev);
+                } else {
+                    input_report_key(input_dev, keycode, 0);
+                    input_sync(input_dev);
+                }
+            }
+
+            /* Desativa coluna (HIGH) */
+            gpiod_set_value(col_gpios->desc[col], 1);
+        }
+
+        /* LED control */
+        if (key_pressed) {
+            gpiod_set_value(ledtec_gpio, 0);  /* LED ACESO */
+        } else {
+            if (time_after(jiffies, led_jiffies + msecs_to_jiffies(500))) {
+                led_state = !led_state;
+                gpiod_set_value(ledtec_gpio, led_state);
+                led_jiffies = jiffies;
+            }
+        }
+
+        cpu_relax();
+    }
+
+    return 0;
 }
 
-/**
- * @brief This function is called, when the module is loaded into the kernel
- */
-static int __init my_init(void) {
-	printk("dt_gpio - Loading the driver...\n");
-	if(platform_driver_register(&my_driver)) {
-		printk("dt_gpio - Error! Could not load driver\n");
-		return -1;
-	}
-	return 0;
+/* Probe */
+static int msa620_probe(struct platform_device *pdev) {
+    int ret, i;
+
+    row_gpios = gpiod_get_array(&pdev->dev, "row", GPIOD_IN);
+    if (IS_ERR(row_gpios))
+        return PTR_ERR(row_gpios);
+
+    col_gpios = gpiod_get_array(&pdev->dev, "col", GPIOD_OUT_HIGH);
+    if (IS_ERR(col_gpios)) {
+        ret = PTR_ERR(col_gpios);
+        goto err_rows;
+    }
+
+    ledtec_gpio = gpiod_get(&pdev->dev, "ledtec", GPIOD_OUT_LOW);
+    if (IS_ERR(ledtec_gpio)) {
+        ret = PTR_ERR(ledtec_gpio);
+        goto err_cols;
+    }
+
+    input_dev = input_allocate_device();
+    if (!input_dev) {
+        ret = -ENOMEM;
+        goto err_led;
+    }
+
+    input_dev->name = "msa620-keyboard";
+    input_dev->phys = "gpio/msa620";
+    input_dev->id.bustype = BUS_HOST;
+    input_dev->evbit[0] = BIT_MASK(EV_KEY);
+
+    for (i = 0; i < NUM_ROWS; i++) {
+        int j;
+        for (j = 0; j < NUM_COLS; j++) {
+            if (key_map[i][j] != -1)
+                set_bit(key_map[i][j], input_dev->keybit);
+        }
+    }
+
+    ret = input_register_device(input_dev);
+    if (ret)
+        goto err_input;
+
+    keyboard_task = kthread_run(keyboard_thread_fn, NULL, "msa620_kb_thread");
+    if (IS_ERR(keyboard_task)) {
+        ret = PTR_ERR(keyboard_task);
+        goto err_input_reg;
+    }
+
+    dev_info(&pdev->dev, "msa620 keyboard driver loaded\n");
+    return 0;
+
+err_input_reg:
+    input_unregister_device(input_dev);
+    input_dev = NULL;
+err_input:
+    input_free_device(input_dev);
+err_led:
+    gpiod_put(ledtec_gpio);
+err_cols:
+    gpiod_put_array(col_gpios);
+err_rows:
+    gpiod_put_array(row_gpios);
+    return ret;
 }
 
-/**
- * @brief This function is called, when the module is removed from the kernel
- */
-static void __exit my_exit(void) {
-	printk("dt_gpio - Unload driver");
-	platform_driver_unregister(&my_driver);
+/* Remove */
+static void msa620_remove(struct platform_device *pdev) {
+    kthread_stop(keyboard_task);
+    input_unregister_device(input_dev);
+    gpiod_put(ledtec_gpio);
+    gpiod_put_array(col_gpios);
+    gpiod_put_array(row_gpios);
+    dev_info(&pdev->dev, "msa620 keyboard driver unloaded\n");
 }
 
-module_init(my_init);
-module_exit(my_exit);
+/* Match */
+static const struct of_device_id msa620_of_match[] = {
+    { .compatible = "keyboard,msa620" },
+    { }
+};
+MODULE_DEVICE_TABLE(of, msa620_of_match);
+
+/* Platform driver */
+static struct platform_driver msa620_driver = {
+    .probe = msa620_probe,
+    .remove = msa620_remove,
+    .driver = {
+        .name = "msa620_keyboard",
+        .of_match_table = msa620_of_match,
+    },
+};
+
+module_platform_driver(msa620_driver);
